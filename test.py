c038f03291b9433ec4fa15e8f3b96036392d3df2
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-05-24 21:59:19 +0200

    Tests

---
+++ test.py
@@ -0,0 +1,39 @@
+from werkzeug.wrappers import response
+from run import app
+
+from socialtracking.users.routes import hash_password
+from socialtracking.users.forms import LoginForm
+import unittest
+from unittest.mock import Mock
+
+class FlaskTestCase(unittest.TestCase):
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Corona Virus Impact Reduction App' in response.data)
+
+
+    def test_register_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type='html/text')
+        self.assertTrue(b'Already have an account' in response.data)
+
+    def test_home_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/homepage', content_type='html/text')
+        self.assertTrue(b'You should be redirected automatically' in response.data)
+
+    def test_hash_password(self):
+        mock = Mock()
+        mock.password.data = '123'
+        self.assertNotEqual("123", hash_password(mock))
+        self.assertEqual("$", hash_password(mock)[0])
+        self.assertEqual("$", hash_password(mock)[3])
+
+if __name__ == '__main__':
+    unittest.main()
